06d8d355b94f679530ea42a145acdc9c14376c77
Rahimjon <abduraximovrahimjon8@gmail.com>
2024-06-26 17:45:11 +0500

    initial

--- src/components/home/Accordion.jsx
+++ src/components/home/Accordion.jsx
@@ -1,83 +1,157 @@
-import React, { useState } from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { FiArrowUpRight } from 'react-icons/fi';
 
-const Accordion = () => {
-  const [activeSection, setActiveSection] = useState(null);
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: 0,
+  paddingBlock: '10px',
+  backgroundColor: '#088269',
+  '&:not(:last-child)': {
+    borderBottom: `1px solid #fff`,
+  },
+  '&::before': {
+    display: 'none',
+  },
+}));
 
-  const handleToggle = (section) => {
-    setActiveSection(activeSection === section ? null : section);
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary expandIcon={<AiOutlinePlus />} {...props} />
+))(({ theme, expanded }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#088269' : '#088269',
+  color: '#fff',
+  fontSize: '1.2rem',
+  flexDirection: 'justify-between',
+  '& .MuiAccordionSummary-expandIconWrapper': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '30px',
+    height: '30px',
+    borderRadius: '50%',
+    backgroundColor: expanded ? '#fff' : '#fff',
+    color: expanded ? '#000' : '#202020',
+    transition: 'background-color 0.3s, color 0.3s',
+  },
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(45deg)',
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  color: '#fff',
+  backgroundColor: '#088269',
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
   };
 
-  const sections = [
-    {
-      title: "О компании",
-      content:
-        "Но синтетическое тестирование, в своём классическом представлении, допускает внедрение поэтапного и последовательного развития общества. В рамках спецификации современных стандартов, сторонники тоталитаризма в науке будут функционально разнесены.",
-    },
-    {
-      title: "Подробнее",
-      content:
-        "Но синтетическое тестирование, в своём классическом представлении, допускает внедрение поэтапного и последовательного развития общества. В рамках спецификации современных стандартов, сторонники тоталитаризма в науке будут функционально разнесены.",
-    },
-    {
-      title: "Преимущества сотрудников",
-      content:
-        "Но синтетическое тестирование, в своём классическом представлении, допускает внедрение поэтапного и последовательного развития общества. В рамках спецификации современных стандартов, сторонники тоталитаризма в науке будут функционально разнесены.",
-    },
-    {
-      title: "Достижения компании",
-      content:
-        "Но синтетическое тестирование, в своём классическом представлении, допускает внедрение поэтапного и последовательного развития общества. В рамках спецификации современных стандартов, сторонники тоталитаризма в науке будут функционально разнесены.",
-    },
-    {
-      title: "Карьерный рост",
-      content:
-        "Но синтетическое тестирование, в своём классическом представлении, допускает внедрение поэтапного и последовательного развития общества. В рамках спецификации современных стандартов, сторонники тоталитаризма в науке будут функционально разнесены.",
-    },
-  ];
-
   return (
-    <div className="bg-[#088269] mt-40 py-12 max-md:py-6">
-      <div className="container flex justify-between py-32 max-md:flex-col">
-        <div className="w-[50%]">
-          <h2 className="text-[30px] text-[#F8F7F3]">Информация о компании</h2>
+    <div className="bg-[--pri] py-[60px] lg:py-[120px]">
+      <div className="container grid grid-cols-1 md:grid-cols-2">
+        <div className="text-white">
+          <p className="text-[18px] md:text-[20px] lg:text-[30px] font-medium leading-[120%] text-white mb-[50px] ">Информация о компании</p>
         </div>
-        <div className="w-[50%] max-md:w-full max-md:my-3">
-          {sections.map((section, index) => (
-            <React.Fragment key={index}>
-              <hr />
-              <div
-                className="flex justify-between items-center py-2 cursor-pointer"
-                onClick={() => handleToggle(index)}
-              >
-                <h3 className="text-[#F8F7F3] text-[18px] py-3">
-                  {section.title}
-                </h3>
-                <p
-                  className={`px-2 border rounded-full duration-150 ${
-                    activeSection === index ? "bg-white rotate-45" : ""
-                  }`}
-                >
-                  <span
-                    className={`text-4xl ${
-                      activeSection === index ? "text-[#088269]" : "text-white"
-                    } bg-transparent`}
-                  >
-                    +
-                  </span>
+
+        <div className="border-t">
+          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+              <Typography>
+                {' '}
+                <p className="text-[18px] ">О компании</p>{' '}
+              </Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+              <Typography>
+                <p className="text-[16px] ">
+                  Но синтетическое тестирование, в своём классическом представлении, допускает
+                  внедрение поэтапного и последовательного развития общества. В рамках спецификации
+                  современных стандартов, сторонники тоталитаризма в науке будут функционально
+                  разнесены.
                 </p>
-              </div>
-              {activeSection === index && (
-                <p className="text-white opacity-75 text-[14px] py-3">
-                  {section.content}
+
+                <button className="mt-[40px] flex text-[14px]  items-center gap-1">
+                  Подробнее <FiArrowUpRight className="text-[16px] " />
+                </button>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+              <Typography>
+                <p className="text-[18px] ">Преимущества сотрудников</p>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+              <p className="text-[16px] ">
+                  Но синтетическое тестирование, в своём классическом представлении, допускает
+                  внедрение поэтапного и последовательного развития общества. В рамках спецификации
+                  современных стандартов, сторонники тоталитаризма в науке будут функционально
+                  разнесены.
                 </p>
-              )}
-            </React.Fragment>
-          ))}
-          <hr />
+                <button className="mt-[40px] flex text-[14px]  items-center gap-1">
+                  Подробнее <FiArrowUpRight className="text-[16px] " />
+                </button>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+              <Typography>
+                <p className="text-[18px] ">Достижения компании</p>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+              <p className="text-[16px] ">
+                  Но синтетическое тестирование, в своём классическом представлении, допускает
+                  внедрение поэтапного и последовательного развития общества. В рамках спецификации
+                  современных стандартов, сторонники тоталитаризма в науке будут функционально
+                  разнесены.
+                </p>
+                <button className="mt-[40px] flex text-[14px]  items-center gap-1">
+                  Подробнее <FiArrowUpRight className="text-[16px] " />
+                </button>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
+              <Typography>
+                <p className="text-[18px] ">Карьерный рост</p>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+              <p className="text-[16px] ">
+                  Но синтетическое тестирование, в своём классическом представлении, допускает
+                  внедрение поэтапного и последовательного развития общества. В рамках спецификации
+                  современных стандартов, сторонники тоталитаризма в науке будут функционально
+                  разнесены.
+                </p>
+                <button className="mt-[40px] flex text-[14px]  items-center gap-1">
+                  Подробнее <FiArrowUpRight className="text-[16px] " />
+                </button>
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </div>
     </div>
   );
-};
-
-export default Accordion;
+}
